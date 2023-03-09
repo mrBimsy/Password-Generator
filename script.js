9cfb8c5c6c82a6f98469e4cdbfb50fe20dc9fbd1
mrBimsy <mrbimsy@gmail.com>
2023-03-09 21:34:27 +0000

    First sketch, not working yet

--- script.js
+++ script.js
@@ -88,17 +88,135 @@ var upperCasedCharacters = [
   "Z",
 ];
 
+// Array of special characters to be included in password
+var specialCharacters = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+let passwordArray = []; // Empty array to store selected characters
+let passwordLength = 0; // Variable to store password lenght
+
 // Function to prompt user for password options
-function getPasswordOptions() {}
+function getPasswordOptions() {
+  passwordLength = prompt(
+    "The number of characters you would like in your password?  (between 10 - 64 characters)"
+  );
+  if (passwordLength < 10 || passwordLength > 64 || isNaN(passwordLength)) {
+    alert("Please enter a number between 10 and 64");
+  }
+  if (confirm("Would you like lowercase letters in your password?")) {
+    choiceArr = choiceArr.concat(lowerCasedCharacters);
+  }
+  if (confirm("Would you like uppercase letters in your password?")) {
+    choiceArr = choiceArr.concat(upperCasedCharacters);
+  }
+  if (confirm("Would you like numeric characters in your password?")) {
+    choiceArr = choiceArr.concat(numericCharacters);
+  }
+  if (confirm("Would you like special characters in your password?")) {
+    choiceArr = choiceArr.concat(specialCharacters);
+  }
+}
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-    const randomIndex = Math.floor(Math.random() * arr.length);
-  }
-  
+  const randomIndex = Math.floor(Math.random() * arr.length);
+}
 
 // Function to generate password with user input
-function generatePassword() {}
+function generatePassword() {
+  var password = "";
+  for (var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.lenght);
+    password = password + choiceArr[randomIndex];
+  }
+}
+return password;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
